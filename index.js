e0c162977c6383bfc2abebbfc47ab1264d91f900
worldofwa <39026053+worldofwa@users.noreply.github.com>
2018-05-06 17:53:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ bot.on('message', function (msg) {
     var chatId = msg.chat.id;
     console.log(msg);
     if(msg.text == 'photo')
-      
-      bot.sendMessage(chatId, "Введи токен ", {caption: "I'm a bot!"});
+
+        bot.sendMessage(chatId, "Введи токен ", {caption: "I'm a bot!"});
     bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
-   notes = console.log(msg);
-       
-    
+        var chatId = msg.chat.id;
+        notes = console.log(msg);
         
-        
-   }
-    if(msg.text == 'token')
     
-     bot.sendMessage(chatId, notes, {caption: "I'm a bot!"});
-   
-});
+    if(msg.text == 'token1')
+
+        bot.sendMessage(chatId, notes, {caption: "I'm a bot!"});
+    
+}
+})
+)

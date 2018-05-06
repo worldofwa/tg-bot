f9ce24f8a39228325de712954367246f72bbe71a
worldofwa <39026053+worldofwa@users.noreply.github.com>
2018-05-06 16:35:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,6 @@ const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot (TOKEN, {polling:true})
 var notes = [];
 
-bot.onText(//напомни (.+) в (.+)/, function (msg, match) {
-    var userId = msg.from.id;
-    var text = match[1];
-    var time = match[2];
-
-    notes.push( { 'uid':userId, 'time':time, 'text':text } );
-
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-});
-
-setInterval(function(){
-    for (var i = 0; i < notes.length; i++){
-        var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-            if ( notes{{i}}['time'] == curDate ) {
-                bot.sendMessage(notes{{i}}['uid'], 'Напоминаю, что вы должны: '+ notes{{i}}['text'] + ' сейчас.');
-                notes.splice(i,1);
-            }
-        }
-},1000);
+bot.on('message', msg=>{
+    bot.sendMessage(msg.chat.id, `hello, bot say: "HI, ${msg.from.first_name}"`)
+})

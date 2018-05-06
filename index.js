84c2773a27ea9249df69d995e1e781e42667d0e6
worldofwa <39026053+worldofwa@users.noreply.github.com>
2018-05-06 17:10:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot (TOKEN, {polling:true})
 var notes = [];
 
-bot.on('message', msg=>{
+
+
+var stateSetName = false;
+
+bot.onText(/\(.*)/, function (msg, match) {
+
+if(!stateSetName)return;
+
+var name = match[1];
+});
+
+bot.onText(/\/setname/, function (msg, match) {
+  
+  var fromId = msg.from.id;
+  bot.sendMessage(fromId, "Введите имя");
+stateSetName =true;
     bot.sendMessage(msg.chat.id, `hello, bot say: "HI, ${msg.from.first_name}"`)
-})
+
+ setTimeout(function(){   bot.sendMessage(fromId, "Отмена!"); stateSetName =false;},30000);
+});
+

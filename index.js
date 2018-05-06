0ab0934556ca20246af360d21af5fd91e5844cc6
worldofwa <39026053+worldofwa@users.noreply.github.com>
2018-05-06 16:03:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const bot = new TelegramBot (TOKEN, {polling:true})
 bot.on('message', msg=>{
     bot.sendMessage(msg.chat.id, `hello, bot say: "HI, ${msg.from.first_name}"`)
 })
+bot.onText(//напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push( { 'uid':userId, 'time':time, 'text':text } );
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});

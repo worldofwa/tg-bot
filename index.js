f4543e6b721d7d93c022b8dd9148ecb50009975d
worldofwa <39026053+worldofwa@users.noreply.github.com>
2018-05-06 17:42:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TOKEN = '535002576:AAHd9_AQB2uYhSivA28TTp0CAZG5CFzs96k'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot (TOKEN, {polling:true})
-var notes = [];
+var notes;
 
 
 bot.on('message', function (msg) {
@@ -9,7 +9,14 @@ bot.on('message', function (msg) {
     console.log(msg);
     if(msg.text == 'photo')
       
-      bot.sendMessage(chatId, "Hello!", {caption: "I'm a bot!"});
-   
+      bot.sendMessage(chatId, "Введи токен ", {caption: "I'm a bot!"});
+    bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+   notes = console.log(msg);
+        bot.sendMessage(chatId, notes, {caption: "I'm a bot!"});
+    
+        
+        
+   })
    
 });
